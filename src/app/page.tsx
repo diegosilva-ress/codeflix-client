d1cd59e0ca1ecbb12887a2c55b6468b2d65f531d
diego-resilva <8083496+diegosilva-ress@users.noreply.github.com>
2024-01-07 15:45:57 -0300

    Criação de testes, ícones e tollbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,26 @@
 import { Inter } from 'next/font/google';
 import Image from 'next/image';
 import Header from '@/app/components/Header';
-
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
+import { PlayIcon } from '@heroicons/react/24/solid';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return(
+  return (
     <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
-      <Header/>
+      <Header />
       <main className='relative pb-24 pl-4 lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
           <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
-            <Image src='/banner5.png'
-                   alt='The Witcher'
-                   fill={true}
-                   className='h-[65vh] object-cover object-top lg:h-[95vh]'/>
+            <Image
+              src='/banner5.png'
+              alt='The Witcher'
+              fill={true}
+              className='h-[65vh] object-cover object-top lg:h-[95vh]'
+            />
           </div>
-          
+
           <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
             The Witcher
           </h1>
@@ -29,14 +32,22 @@ export default function Home() {
         </div>
 
         <div className='flex space-x-3'>
-          <button className='md:text-xl; flex cursor-pointer items-center gap-x-2
+          <button
+            className='md:text-xl; flex cursor-pointer items-center gap-x-2
           rounded bg-white px-5 py-1.5 text-sm font-semibold text-black
-          transition hover:opacity-75 md:px-8 md:py-2.5'>Play
+          transition hover:opacity-75 md:px-8 md:py-2.5'
+          >
+            <PlayIcon className='h-6' />
+            Play
           </button>
 
-          <button className='md:text-xl; flex cursor-pointer items-center gap-x-2
+          <button
+            className='md:text-xl; flex cursor-pointer items-center gap-x-2
           rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black
-          transition hover:opacity-75 md:px-8 md:py-2.5'>More Info
+          transition hover:opacity-75 md:px-8 md:py-2.5'
+          >
+            <InformationCircleIcon className='h-6' />
+            More Info
           </button>
         </div>
       </main>

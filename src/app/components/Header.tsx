637f80b40335d77ac823c208770aa8d65991023b
diego-resilva <8083496+diegosilva-ress@users.noreply.github.com>
2024-01-07 14:55:54 -0300

    Criação de componentes

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,47 +1,24 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { UserProfile } from '@/app/components/UserProfile';
+import { Logo } from '@/app/components/Logo';
+import { NavLinks } from '@/app/components/NavLinks';
+import { useScroll } from '@/app/hooks/UseScroll';
 
 export default function Header() {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 0);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  const isScrolled = useScroll();
 
   return (
     <header className={`${isScrolled && 'bg-black'}
-      fixed top-0 z-50 flex w-full items-center justify-between transition-all px-4 py-4 lg:px-10 lg:py-6`}>
+      fixed top-0 z-50 flex w-full items-center justify-between p-2 transition-all lg:px-10 lg:py-4`}>
 
       <div className='flex items-center space-x-2 md:space-x-8'>
-        <img src='https://rb.gy/ulxxee' alt='netflix-logo' width={120} height={120} />
-        <ul className='hidden md:flex md:space-x-4'>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movie</li>
-          <li>Latest</li>
-        </ul>
+        <Logo />
+        <NavLinks />
       </div>
 
-      <div className='flex items-center space-x-4'>
-        <p className='hidden cursor-not-allowed lg:inline'>
-          Kids
-        </p>
-
-        <img src='https://rb.gy/g1pwyx'
-             alt='profile'
-             className='cursor-pointer rounded'/>
-
-      </div>
-
+      <UserProfile />
     </header>
-  )
+  );
 }

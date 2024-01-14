55ef5149882e6c3382e23e1daac2a39000fb1c67
diego-resilva <8083496+diegosilva-ress@users.noreply.github.com>
2024-01-14 16:30:44 -0300

    Implementação da página de login

---
+++ src/app/auth/InputField.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export type InputFieldProps = {
+  id: string;
+  label: string;
+  type: string;
+  placeholder: string;
+  // name: string;
+  // value: string;
+  // onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  // error?: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({
+  id,
+  label,
+  type,
+  placeholder,
+}) => {
+  return (
+    <div className='flex flex-col space-y-1'>
+      <label htmlFor={label}>{label}</label>
+      <input
+        className='rounded-lg border-gray-600 bg-gray-700 px-4 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-red-500'
+        type={type}
+        id={id}
+        name={id}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import LoginForm from '@/app/auth/login/Login';
+
+export default function LoginPage() {
+  return <LoginForm />;
+}

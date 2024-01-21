c0029d721958973072f7be42da36de51ad22e241
diego-resilva <8083496+diegosilva-ress@users.noreply.github.com>
2024-01-21 16:52:59 -0300

    Implementação do Banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,17 @@ import { Inter } from 'next/font/google';
 import Header from '@/app/components/Header';
 import { MovieRow } from '@/app/components/MovieRow';
 import { Banner } from '@/app/components/Banner';
+import { getFeaturedMovie, getMoviesByGenre } from './service/MovieService';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default async function Home() {
+  const featuredMovie = await getFeaturedMovie('104');
   return (
     <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
       <Header />
-      <Banner />
       <main className='relative pb-24 pl-4 lg:pl-16'>
+        <Banner movie={featuredMovie} />
         <MovieRow sectionTitle='Trending row' />
         <MovieRow sectionTitle='Top rated' />
       </main>
